f2676a3e4a8537860e3e4f2d9736f740464f9987
reasonov1k <76642392+reasonov1k@users.noreply.github.com>
2022-02-22 16:55:45 +1000

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="rate-page">
+    <h2 class="rate__title">Выберите подходящий Вам тариф qoob.store</h2>
+    <div class="rate__info info">
+      <p class="info__date">Лицензия Вашего аккаунта окончена 24.12.2021</p>
+      <p class="info__rate">Тариф: <b>“Стандарт”</b></p>
+      <p class="info__days">6 дней назад</p>
+    </div>
+    <p class="rate__recommendation">Рекомендуем вам опробовать тариф <b>“Базовый”</b></p>
+
+    <ul class="card-list">
+      <li v-for="(item, index) in cardList" :key="index">
+        <card-component
+          :parentData="item"
+        />
+      </li>
+    </ul>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import cardComponent from './components/card-component';
+import { cardList } from './data.json';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    cardComponent: cardComponent
+  },
+  setup() {
+    return { cardList };
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="./assets/fonts/style.css"></style>
+<style src="./assets/css/style.css"></style>
